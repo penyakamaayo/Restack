b6c1586316433ca1c2870cef28f565027c9b103d
Tepen <elcabron.penyah@gmail.com>
2020-06-30 14:51:32 +0800

    annotated models!

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id             :bigint           not null, primary key
+#  user_id        :bigint
+#  title          :string
+#  body           :text
+#  upvotes        :integer
+#  downvotes      :integer
+#  total_comments :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Post < ApplicationRecord
 
 	belongs_to :user
