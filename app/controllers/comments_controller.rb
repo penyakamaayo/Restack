0dba2595f30913d8361ab9d2ec1960649d51d67d
Tepen <elcabron.penyah@gmail.com>
2020-07-01 22:16:19 +0800

    added comments section

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,15 +25,12 @@
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to post_path(@comment.post_id)
+    else 
+      render 'new'
     end
   end
 
