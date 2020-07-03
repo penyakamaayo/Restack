c5b8aed614b0635c8afa3e87201bbc4784a990cd
Tepen <elcabron.penyah@gmail.com>
2020-07-04 00:03:26 +0800

    cleaned up unused codes, converted erb files to haml

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -27,37 +27,11 @@
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_reply
       @reply = Reply.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def reply_params
       params.require(:reply).permit(:body, :user_id, :comment_id)
     end
 end
-
-  # # PATCH/PUT /comments/1
-  # # PATCH/PUT /comments/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @comment.update(comment_params)
-  #       format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @comment }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @comment.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # # DELETE /comments/1
-  # # DELETE /comments/1.json
-  # def destroy
-  #   @comment.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
